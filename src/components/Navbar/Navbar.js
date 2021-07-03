d7fcbc3c1c00404a011656a47e94e8df35f94819
tediko <tediko123@wp.pl>
2021-07-03 11:58:06 +0200

    Add: windowWidth state and listener to watch for resize and update windowWidth

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,21 @@
 import { Header, Logo, Container } from './NavbarStyles';
 import NavMobile from './NavMobile';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', () =>
+            setWindowWidth(window.innerWidth)
+        );
+    }, [windowWidth]);
+
     return (
         <Header>
             <Container>
                 <Logo to="/">The Planets</Logo>
-                <NavMobile />
+                <NavMobile windowWidth={windowWidth} />
             </Container>
         </Header>
     );
