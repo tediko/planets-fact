8b13873e344a177544e4533a7a6d8cbfcc4d9ebe
tediko <tediko123@wp.pl>
2021-07-03 14:00:40 +0200

    Add: NavDesktop component

---
+++ src/components/Navbar/NavDesktop.js
@@ -0,0 +1,14 @@
+import { Nav, List } from './NavbarStyles';
+import ItemControler from './ItemControler';
+
+const NavDesktop = () => {
+    return (
+        <Nav>
+            <List>
+                <ItemControler />
+            </List>
+        </Nav>
+    );
+};
+
+export default NavDesktop;
