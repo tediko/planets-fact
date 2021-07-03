35684903f4c8350194eaadc6b4b59b5913e6c40e
tediko <tediko123@wp.pl>
2021-07-03 14:13:33 +0200

    Update: data sectionColor

--- src/components/Navbar/data.js
+++ src/components/Navbar/data.js
@@ -3,40 +3,48 @@ export const planets = [
         id: 0,
         name: 'Mercury',
         color: 'hsl(196, 83%, 93%)',
+        sectionColor: 'hsl(194, 48%, 49%)',
     },
     {
         id: 1,
         name: 'Venus',
         color: 'hsl(39, 88%, 73%)',
+        sectionColor: 'hsl(33, 82%, 61%)',
     },
     {
         id: 2,
         name: 'Earth',
         color: 'hsl(238, 99%, 66%)',
+        sectionColor: 'hsl(263, 67%, 51%)',
     },
     {
         id: 3,
         name: 'Mars',
         color: 'hsl(12, 100%, 64%)',
+        sectionColor: 'hsl(10, 63%, 51%)',
     },
     {
         id: 4,
         name: 'Jupiter',
         color: 'hsl(27, 75%, 70%)',
+        sectionColor: 'hsl(2, 68%, 53%)',
     },
     {
         id: 5,
         name: 'Saturn',
         color: 'hsl(40, 96%, 70%)',
+        sectionColor: 'hsl(17, 73%, 46%)',
     },
     {
         id: 6,
         name: 'Uranus',
         color: 'hsl(168, 82%, 67%)',
+        sectionColor: 'hsl(169, 73%, 44%)',
     },
     {
         id: 7,
         name: 'Neptune',
         color: 'hsl(222, 95%, 63%)',
+        sectionColor: 'hsl(222, 87%, 56%)',
     },
 ];
