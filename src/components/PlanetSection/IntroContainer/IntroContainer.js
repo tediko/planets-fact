e89c27c466cf6519fa4ad2ab66a6d341676fd22d
tediko <tediko123@wp.pl>
2021-07-04 14:29:11 +0200

    Add: IntroContainer component with styles

---
+++ src/components/PlanetSection/IntroContainer/IntroContainer.js
@@ -0,0 +1,33 @@
+import {
+    Intro,
+    Title,
+    Text,
+    SourceContainer,
+    Span,
+    Link,
+} from './IntroContainerStyles';
+import Icon from '../../Icon/Icon';
+
+const IntroContainer = () => {
+    return (
+        <Intro>
+            <Title>Earth</Title>
+            <Text>
+                Third planet from the Sun and the only known planet to harbor
+                life. About 29.2% of Earth's surface is land with remaining
+                70.8% is covered with water. Earth's distance from the Sun,
+                physical properties and geological history have allowed life to
+                evolve and thrive.
+            </Text>
+            <SourceContainer>
+                <Span>Source: </Span>
+                <Link href="http://wikipedia.com">
+                    Wikipedia{' '}
+                    <Icon name="source" size={12} color="hsl(240, 6%, 54%)" />
+                </Link>
+            </SourceContainer>
+        </Intro>
+    );
+};
+
+export default IntroContainer;
---
+++ src/components/PlanetSection/IntroContainer/IntroContainerStyles.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const Intro = styled.div`
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    padding: 0 24px;
+    margin-bottom: 28px;
+
+    @media (min-width: 768px) {
+        grid-area: intro;
+        align-items: start;
+        padding: 0;
+    }
+
+    @media (min-width: 1025px) {
+        align-self: end;
+        margin: 60px 0 39px 0;
+    }
+`;
+
+export const Title = styled.h1`
+    color: ${(props) => props.theme.colors.white};
+    font-size: 2.5rem;
+    font-family: ${(props) => props.theme.fonts.secondary};
+    font-weight: 500;
+    line-height: 100%;
+    text-transform: uppercase;
+    margin-bottom: 16px;
+
+    @media (min-width: 768px) {
+        font-size: 3rem;
+        margin-bottom: 24px;
+    }
+
+    @media (min-width: 1025px) {
+        font-size: 5rem;
+        margin-bottom: 34px;
+    }
+`;
+
+export const Text = styled.p`
+    font-size: 0.6875rem;
+    line-height: 22px;
+    color: ${(props) => props.theme.colors.whiteAlpha75};
+    text-align: center;
+    margin-bottom: 10px;
+
+    @media (min-width: 768px) {
+        text-align: left;
+        margin-bottom: 32px;
+    }
+
+    @media (min-width: 1025px) {
+        font-size: 0.875rem;
+        line-height: 25px;
+        margin-bottom: 24px;
+    }
+`;
+
+export const SourceContainer = styled.div`
+    display: flex;
+    gap: 5px;
+`;
+
+export const Span = styled.span`
+    color: ${(props) => props.theme.colors.grayLight};
+    font-size: 0.75rem;
+    line-height: 25px;
+
+    @media (min-width: 1025px) {
+        font-size: 0.875rem;
+    }
+`;
+
+export const Link = styled.a`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-size: 0.75rem;
+    font-weight: 600;
+    line-height: 25px;
+    text-decoration: underline;
+    color: ${(props) => props.theme.colors.grayLight};
+
+    @media (min-width: 1025px) {
+        font-size: 0.875rem;
+        gap: 8px;
+    }
+`;
