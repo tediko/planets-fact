266c202908b38fff5791fe3a27bb1e0430085136
tediko <tediko123@wp.pl>
2021-07-04 14:39:57 +0200

    Add: Temporarily added route to PlanetSection component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Provider from '../Provider/Provider';
 import Wrapper from '../Wrapper/Wrapper';
 import Navbar from '../Navbar/Navbar';
+import PlanetSection from '../PlanetSection/PlanetSection';
 
 const App = () => {
     return (
         <Provider>
             <Wrapper>
-                <Navbar />
+                <Router>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/">
+                            <PlanetSection />
+                        </Route>
+                    </Switch>
+                </Router>
             </Wrapper>
         </Provider>
     );
