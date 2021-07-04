9070c88f3b289fd2b3410c9f89938490b563ece8
tediko <tediko123@wp.pl>
2021-07-04 15:19:16 +0200

    Add: background-image for Wrapper

--- src/components/Wrapper/WrapperStyles.js
+++ src/components/Wrapper/WrapperStyles.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
+import starsBackground from '../../assets/images/background-stars.svg';
 
 export const Container = styled.div`
     position: relative;
     min-height: 100vh;
-    background: ${(props) => props.theme.colors.black};
+    background-color: ${(props) => props.theme.colors.black};
+    background-image: url(${starsBackground});
+    background-size: 100%;
 `;
