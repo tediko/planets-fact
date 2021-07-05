a17760252bb644c03d8c33f0b68a526870df623a
tediko <tediko123@wp.pl>
2021-07-05 14:39:34 +0200

    Add: Import useReplaceInfo hook and assign retured values to Components

--- src/components/PlanetSection/PlanetSection.js
+++ src/components/PlanetSection/PlanetSection.js
@@ -1,17 +1,33 @@
 import { Section, Container } from './PlanetSectionStyles';
+import useReplaceInfo from './useReplaceInfo';
 import Tabs from './Tabs/Tabs';
 import Illustration from './Illustration/Illustration';
 import Intro from './Intro/Intro';
 import Info from './Info/Info';
 
-const PlanetSection = () => {
+const PlanetSection = ({ planetData }) => {
+    const [handleClick, currentData, currentTab, isChanging] =
+        useReplaceInfo(planetData);
+
     return (
         <Section>
             <Container>
-                <Tabs />
-                <Illustration />
-                <Intro />
-                <Info />
+                <Tabs
+                    planetData={planetData}
+                    handleClick={handleClick}
+                    currentTab={currentTab}
+                />
+                <Illustration
+                    planetData={planetData}
+                    currentData={currentData}
+                    isChanging={isChanging}
+                />
+                <Intro
+                    planetData={planetData}
+                    currentData={currentData}
+                    isChanging={isChanging}
+                />
+                <Info planetData={planetData} />
             </Container>
         </Section>
     );
