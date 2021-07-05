62d687a102d4c54c769a7ff9ce883663ec43470f
tediko <tediko123@wp.pl>
2021-07-05 14:33:25 +0200

    Add: Take planetData, handleClick, currentTab properties and use them on Tab

--- src/components/PlanetSection/Tabs/Tabs.js
+++ src/components/PlanetSection/Tabs/Tabs.js
@@ -1,11 +1,32 @@
 import { Tabs, Tab } from './TabsStyles';
 
-const TabsContainer = () => {
+const TabsContainer = ({ planetData, handleClick, currentTab }) => {
     return (
         <Tabs>
-            <Tab isActive>Overview</Tab>
-            <Tab>Structure</Tab>
-            <Tab>Surface</Tab>
+            <Tab
+                data-type="overview"
+                planetData={planetData}
+                onClick={handleClick}
+                isActive={currentTab === 'overview' ? true : false}
+            >
+                Overview
+            </Tab>
+            <Tab
+                data-type="structure"
+                planetData={planetData}
+                onClick={handleClick}
+                isActive={currentTab === 'structure' ? true : false}
+            >
+                Structure
+            </Tab>
+            <Tab
+                data-type="geology"
+                planetData={planetData}
+                onClick={handleClick}
+                isActive={currentTab === 'geology' ? true : false}
+            >
+                Surface
+            </Tab>
         </Tabs>
     );
 };
