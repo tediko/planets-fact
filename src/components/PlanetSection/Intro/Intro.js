11565fa33c531a0a1ad600aca0d46b27c8a38255
tediko <tediko123@wp.pl>
2021-07-05 14:36:05 +0200

    Add: Take props and use them across Component

--- src/components/PlanetSection/Intro/Intro.js
+++ src/components/PlanetSection/Intro/Intro.js
@@ -1,21 +1,15 @@
 import { Intro, Title, Text, SourceContainer, Span, Link } from './IntroStyles';
 import Icon from '../../Icon/Icon';
 
-const IntroContainer = () => {
+const IntroContainer = ({ planetData, currentData, isChanging }) => {
     return (
         <Intro>
-            <Title>Earth</Title>
-            <Text>
-                Third planet from the Sun and the only known planet to harbor
-                life. About 29.2% of Earth's surface is land with remaining
-                70.8% is covered with water. Earth's distance from the Sun,
-                physical properties and geological history have allowed life to
-                evolve and thrive.
-            </Text>
+            <Title>{planetData.name}</Title>
+            <Text isChanging={isChanging}>{currentData.content}</Text>
             <SourceContainer>
                 <Span>Source: </Span>
-                <Link href="http://wikipedia.com">
-                    Wikipedia{' '}
+                <Link href={currentData.source}>
+                    Wikipedia
                     <Icon name="source" size={12} color="hsl(240, 6%, 54%)" />
                 </Link>
             </SourceContainer>
