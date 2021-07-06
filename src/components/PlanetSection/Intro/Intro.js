b397705be631bd1d93d1485b900d53ecf0fc3b9c
tediko <tediko123@wp.pl>
2021-07-06 13:57:50 +0200

    Add: containerVariants object and use it for Intro props

--- src/components/PlanetSection/Intro/Intro.js
+++ src/components/PlanetSection/Intro/Intro.js
@@ -2,8 +2,30 @@ import { Intro, Title, Text, SourceContainer, Span, Link } from './IntroStyles';
 import Icon from '../../Icon/Icon';
 
 const IntroContainer = ({ planetData, currentData, isChanging }) => {
+    const containerVariants = {
+        hidden: {
+            opacity: 0,
+            x: 50,
+        },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: { delay: 1.5, duration: 1.5 },
+        },
+        exit: {
+            opacity: 0,
+            x: 50,
+            transition: { duration: 1 },
+        },
+    };
+
     return (
-        <Intro>
+        <Intro
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
             <Title>{planetData.name}</Title>
             <Text isChanging={isChanging}>{currentData.content}</Text>
             <SourceContainer>
