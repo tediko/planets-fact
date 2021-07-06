6ca38f849531aa04e7179a04ab259b57ed84bdee
tediko <tediko123@wp.pl>
2021-07-06 13:56:01 +0200

    Add: containerVariants object and use it for Info props

--- src/components/PlanetSection/Info/Info.js
+++ src/components/PlanetSection/Info/Info.js
@@ -1,8 +1,30 @@
 import { Info, Item, Heading, Desc } from './InfoStyles';
 
 const InfoContainer = ({ planetData }) => {
+    const containerVariants = {
+        hidden: {
+            opacity: 0,
+            y: 25,
+        },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { delay: 1.5, duration: 1.5 },
+        },
+        exit: {
+            opacity: 0,
+            y: 25,
+            transition: { duration: 1 },
+        },
+    };
+
     return (
-        <Info>
+        <Info
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
             <Item>
                 <Heading>Rotation Time</Heading>
                 <Desc>{planetData.rotation}</Desc>
