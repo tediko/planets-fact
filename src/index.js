9a4e4e5bcdb913617896c915e6a16b71406ae87e
tediko <tediko123@wp.pl>
2021-07-06 13:46:26 +0200

    Add: Import BrowserRouter and wrap App into that

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
