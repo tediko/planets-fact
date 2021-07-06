2a7d4afb61d1fd2f294767d09c51a2d0d144abc1
tediko <tediko123@wp.pl>
2021-07-06 13:53:47 +0200

    Add: containerVariants object and use it for framer-motion props

--- src/components/PlanetSection/Illustration/Illustration.js
+++ src/components/PlanetSection/Illustration/Illustration.js
@@ -1,8 +1,36 @@
 import { Illustration, Image } from './IllustrationStyles';
 
 const IllustrationContainer = ({ planetData, currentData, isChanging }) => {
+    const containerVariants = {
+        hidden: {
+            opacity: 0,
+            scale: 0.5,
+            rotate: '20deg',
+            x: 200,
+        },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            rotate: 0,
+            x: 0,
+            transition: { delay: 1, duration: 1.5 },
+        },
+        exit: {
+            opacity: 0,
+            scale: 0.5,
+            rotate: '-20deg',
+            x: -200,
+            transition: { duration: 1 },
+        },
+    };
+
     return (
-        <Illustration>
+        <Illustration
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
             <Image
                 image={currentData.image}
                 geo={currentData.geo ? currentData.geo : false}
