a6065755fd1488171c16b2890922bc022498cdd2
tediko <tediko123@wp.pl>
2021-07-06 14:53:52 +0200

    Add: starsBackground to box-shadow and background-image to Contariner

--- src/components/Wrapper/WrapperStyles.js
+++ src/components/Wrapper/WrapperStyles.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
-import starsBackground from '../../assets/images/background-stars.svg';
+import { starsBackground } from './starsBackground';
 
 export const Container = styled.div`
     position: relative;
     min-height: 100vh;
-    background-color: ${(props) => props.theme.colors.black};
-    background-image: url(${starsBackground});
-    background-size: 100%;
+    background-image: radial-gradient(
+        ellipse at bottom,
+        #1c2837 0%,
+        #050608 100%
+    );
     overflow: hidden;
+
+    &::after {
+        content: '';
+        position: absolute;
+        height: 2px;
+        width: 2px;
+        top: -2px;
+        background: white;
+        box-shadow: ${starsBackground};
+        border-radius: 100px;
+    }
 `;
